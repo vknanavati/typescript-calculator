65a92014bc475169a3e4dc024ca910f42da25050
vknanavati <vimin.nanavati@gmail.com>
2025-03-05 17:12:02 -0500

    added types for amount, total, and name state variables and type for handleSubmitBudget.

--- src/types.ts
+++ src/types.ts
@@ -9,3 +9,5 @@ export type SetName = React.Dispatch<React.SetStateAction<string>>
 export type SetAmount = React.Dispatch<React.SetStateAction<string>>
 
 export type SetTotal = React.Dispatch<React.SetStateAction<string>>
+
+export type HandleSubmitBudget = (e: React.FormEvent<HTMLFormElement>) => void
