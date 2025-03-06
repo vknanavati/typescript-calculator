307007bdb87ee3467b91f3602f99c7f7ceec49e6
vknanavati <vimin.nanavati@gmail.com>
2025-03-06 17:22:58 -0500

    created handleAddExpense function to open dialog form, passed as prop to BudetCard, created isOpenDialog for setting state of Expense Form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
- import { useState } from 'react';
+import { useState } from 'react';
 import { Container } from '@mui/material';
 import { CreateBudget } from './components/CreateBudget';
-import { BudgetCard } from './components/BudgetCard'
+import { BudgetCard } from './components/BudgetCard';
+import { AddExpenseForm } from './components/AddExpenseForm';
+import { HandleAddExpense } from './types';
 
 function App() {
 
   const [name, setName] = useState("")
   const [amount, setAmount] = useState("")
   const [total, setTotal] = useState("")
+
+  const [isOpenDialog, setIsOpenDialog] = useState(false)
+
+  const handleAddExpense: HandleAddExpense = () => {
+    setIsOpenDialog(true)
+
+  }
+
   return (
-   <Container sx={{mt: 5}}>
-    <CreateBudget setName={setName} setAmount={setAmount} setTotal={setTotal}/>
-    {name && amount && (
-      <BudgetCard name={name} amount={amount} total={total}/>
-    )}
+    <Container sx={{mt: 5}}>
+      <CreateBudget setName={setName} setAmount={setAmount} setTotal={setTotal}/>
+      {name && amount && (
+        <BudgetCard name={name} amount={amount} total={total} onAddExpense={handleAddExpense}/>
+      )}
+      <AddExpenseForm open={isOpenDialog}/>
    </Container>
   );
 }
