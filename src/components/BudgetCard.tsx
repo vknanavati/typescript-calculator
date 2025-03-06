499e0f309f5473eec94a1c31cb86acbe9bc8fc0f
vknanavati <vimin.nanavati@gmail.com>
2025-03-06 17:23:46 -0500

    passed handleAddExpress as onAddExpense.

--- src/components/BudgetCard.tsx
+++ src/components/BudgetCard.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { Amount, Name, Total } from '../types';
+import { Box, Typography, Button } from '@mui/material';
+import { Amount, Name, Total, HandleAddExpense } from '../types';
 
 interface BudgetCardProps {
     name: Name
     amount: Amount
     total: Total
+    onAddExpense: HandleAddExpense
 }
-export function BudgetCard({name, amount, total}: BudgetCardProps) {
+export function BudgetCard({name, amount, total, onAddExpense}: BudgetCardProps) {
+
+
   return (
     <Box
         sx={{
@@ -17,9 +19,19 @@ export function BudgetCard({name, amount, total}: BudgetCardProps) {
             mt: 5
         }}
     >
-        <Typography>{name}</Typography>
-        <Typography>{amount}/{total}</Typography>
+        <Box display={'flex'} justifyContent={'space-around'} gap={5}>
+            <Typography>{name}</Typography>
+            <Typography>{amount}/{total}</Typography>
+        </Box>
 
+        <Box display={'flex'} justifyContent={'right'} mt={2}>
+            <Button
+                variant='contained'
+                onClick={onAddExpense}
+            >
+                Add Expense
+            </Button>
+        </Box>
     </Box>
   )
 }
