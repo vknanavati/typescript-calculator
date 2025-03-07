cc65c77bcd658042de1a47a8f06f858f4e56cea8
vknanavati <vimin.nanavati@gmail.com>
2025-03-07 15:55:51 -0500

    passed onCloseExpense prop and set to onClick in Cancel Button.

--- src/components/AddExpenseForm.tsx
+++ src/components/AddExpenseForm.tsx
@@ -1,12 +1,25 @@
-import { Dialog } from "@mui/material";
-import { DialogOpenState } from "../types";
+import { Dialog, DialogContent, DialogTitle, TextField, Box, Button, DialogActions } from "@mui/material";
+import { DialogOpenState, HandleCloseExpense } from "../types";
 
 interface AddExpenseFormProps {
     open: DialogOpenState
+    onCloseExpense: HandleCloseExpense
 }
 
-export function AddExpenseForm({open}: AddExpenseFormProps) {
+export function AddExpenseForm({open, onCloseExpense}: AddExpenseFormProps) {
   return (
-    <Dialog open={open}></Dialog>
+    <Dialog open={open}>
+        <DialogTitle>Expense</DialogTitle>
+        <DialogContent>
+            <Box display={'flex'} gap={6}>
+                <TextField label="Description" variant="standard"/>
+                <TextField label="Amount" variant="standard"/>
+            </Box>
+        </DialogContent>
+        <DialogActions>
+                <Button>Add</Button>
+                <Button onClick={onCloseExpense}>Cancel</Button>
+        </DialogActions>
+    </Dialog>
   )
 }
