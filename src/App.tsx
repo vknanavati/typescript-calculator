f214be6e122ddda5d19ef6c16ee9e63b1bdfc625
vknanavati <vimin.nanavati@gmail.com>
2025-03-10 00:17:38 -0400

    added expenses useState and updated saveExpense function.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,33 @@ import { Container } from '@mui/material';
 import { CreateBudget } from './components/CreateBudget';
 import { BudgetCard } from './components/BudgetCard';
 import { AddExpenseForm } from './components/AddExpenseForm';
-import { HandleAddExpense, HandleCloseExpense } from './types';
+import { HandleAddExpense, HandleCloseExpense, HandleSaveExpense, SetExpenses, Expense } from './types';
 
 function App() {
 
   const [name, setName] = useState("")
   const [amount, setAmount] = useState("")
   const [total, setTotal] = useState("")
+
+  const [expenseDescription, setExpenseDescription] = useState("")
+  const [expenseAmount, setExpenseAmount] = useState("")
+
+  const [expenses, setExpenses] = useState<Expense[]>([])
+
+  const handleSaveExpense: HandleSaveExpense = () => {
+
+    if (expenseAmount && expenseDescription) {
+      setExpenses(prev => [...prev, {description: expenseDescription, amount: expenseAmount}])
+    }
+    const updatedAmount = Number(amount) - Number(expenseAmount);
+    setAmount(updatedAmount.toString());
+
+    setExpenseDescription("")
+    setExpenseAmount("")
+    setIsOpenDialog(false)
+  }
+
+
 
   const [isOpenDialog, setIsOpenDialog] = useState(false)
 
@@ -25,9 +45,21 @@ function App() {
     <Container sx={{mt: 5}}>
       <CreateBudget setName={setName} setAmount={setAmount} setTotal={setTotal}/>
       {name && amount && (
-        <BudgetCard name={name} amount={amount} total={total} onAddExpense={handleAddExpense}/>
+        <BudgetCard
+          name={name}
+          amount={amount}
+          total={total}
+          onAddExpense={handleAddExpense}
+          expenses={expenses}
+        />
       )}
-      <AddExpenseForm open={isOpenDialog} onCloseExpense={handleCloseExpense}/>
+      <AddExpenseForm
+        open={isOpenDialog}
+        onCloseExpense={handleCloseExpense}
+        setExpenseDescription={setExpenseDescription}
+        setExpenseAmount={setExpenseAmount}
+        onSaveExpense={handleSaveExpense}
+      />
    </Container>
   );
 }
