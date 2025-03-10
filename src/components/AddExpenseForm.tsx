768cdb1ed3f59296d9dda0cb080acad5cc36d15d
vknanavati <vimin.nanavati@gmail.com>
2025-03-09 23:43:45 -0400

    added onClick function to Add Expense Button, onChange to textfields.

--- src/components/AddExpenseForm.tsx
+++ src/components/AddExpenseForm.tsx
@@ -1,24 +1,34 @@
 import { Dialog, DialogContent, DialogTitle, TextField, Box, Button, DialogActions } from "@mui/material";
-import { DialogOpenState, HandleCloseExpense, SetExpenseDescription } from "../types";
+import { DialogOpenState, HandleCloseExpense, SetExpenseDescription, SetExpenseAmount, HandleSaveExpense } from "../types";
 
 interface AddExpenseFormProps {
     open: DialogOpenState
     onCloseExpense: HandleCloseExpense
     setExpenseDescription: SetExpenseDescription
+    setExpenseAmount: SetExpenseAmount
+    onSaveExpense: HandleSaveExpense
 }
 
-export function AddExpenseForm({open, onCloseExpense, setExpenseDescription}: AddExpenseFormProps) {
+export function AddExpenseForm({open, onCloseExpense, setExpenseDescription, setExpenseAmount, onSaveExpense}: AddExpenseFormProps) {
   return (
     <Dialog open={open}>
         <DialogTitle>Expense</DialogTitle>
         <DialogContent>
             <Box display={'flex'} gap={6}>
-                <TextField label="Description" variant="standard"/>
-                <TextField label="Amount" variant="standard"/>
+                <TextField
+                    label="Description"
+                    variant="standard"
+                    onChange={(e) => setExpenseDescription(e.target.value)}
+                />
+                <TextField
+                    label="Amount"
+                    variant="standard"
+                    onChange={(e) => setExpenseAmount(e.target.value)}
+                />
             </Box>
         </DialogContent>
         <DialogActions>
-                <Button>Add</Button>
+                <Button onClick={onSaveExpense}>Add</Button>
                 <Button onClick={onCloseExpense}>Cancel</Button>
         </DialogActions>
     </Dialog>
