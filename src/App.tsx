b8543a5866fa92e3ce3f076f24ef4d449e2586b9
vknanavati <vimin.nanavati@gmail.com>
2025-03-12 11:59:52 -0400

    removed unused state variables, updated handleSubmitBudget, handleAddExpense, handleSaveExpense, created addedBudget object.

--- src/App.tsx
+++ src/App.tsx
@@ -7,63 +7,98 @@ import { HandleAddExpense, HandleCloseExpense, HandleSaveExpense, SetExpenses, E
 
 function App() {
 
-  const [name, setName] = useState("")
-  const [amount, setAmount] = useState("")
+  //in CreateBudget user enters budget name and budget amount
+  const [inputName, setInputName] = useState("")
+  const [inputAmount, setInputAmount] = useState("")
+
+  //inputAmount is set to total
   const [total, setTotal] = useState("")
+
+  //inputName and inputAmount are added to addedBudgets2
+  const [addedBudgets, setAddedBudgets] = useState<BudgetCardInfo[]>([])
 
   const [expenseDescription, setExpenseDescription] = useState("")
   const [expenseAmount, setExpenseAmount] = useState("")
 
   const [expenses, setExpenses] = useState<Expense[]>([])
 
-  const [addedBudgets, setAddedBudgets] = useState<BudgetCardInfo[]>([])
+  //array of objects of each budget card info. Includes name, budget, expenses
 
+  //used in handleAddExpense to keep track of which budget you are adding to
+  const [selectedBudget, setSelectedBudget] = useState("")
 
-  const [inputName, setInputName] = useState("")
-  const [inputAmount, setInputAmount] = useState("")
+  const [isOpenDialog, setIsOpenDialog] = useState(false)
 
+  //User enters budget name and amount to create a new budget card
+  //inputName and inputAmount are assigned to name and budget keys in the addedBudgets object
+  //addedBudgets object is need to store different budgets created by the user so they can be mapped to their own cards
   const handleSubmitBudget: HandleSubmitBudget = (e) => {
+
       e.preventDefault();
-      setName(inputName);
-      setAmount(inputAmount);
+
+      if (inputName && inputAmount) {
+        setAddedBudgets(prev => [...prev, {
+          name: inputName,
+          budget: inputAmount,
+          expenses: []
+        }]);
+      }
+
+      console.log("addedBudgets: ", addedBudgets)
+
+      // setName(inputName);
+      // setAmount(inputAmount);
       setTotal(inputAmount);
+
       setInputName("");
       setInputAmount("");
   }
 
-  const handleSaveExpense: HandleSaveExpense = () => {
-
-    if (expenseAmount && expenseDescription) {
-      setExpenses(prev => [...prev, {description: expenseDescription, amount: expenseAmount}])
-    }
-    const updatedAmount = Number(amount) - Number(expenseAmount);
-
-    console.log("added expenses:", expenses)
-    setAmount(updatedAmount.toString());
-
-    setExpenseDescription("")
-    setExpenseAmount("")
-    setIsOpenDialog(false)
-  }
-
-
-
-  const [isOpenDialog, setIsOpenDialog] = useState(false)
-
-  const handleAddExpense: HandleAddExpense = () => {
-    setIsOpenDialog(true)
+  const handleAddExpense: HandleAddExpense = (budgetName) => {
+    setSelectedBudget(budgetName);
+    console.log("budgetName: ", budgetName)
+    console.log("selectedBudget: ", selectedBudget)
+    setIsOpenDialog(true);
   }
 
   const handleCloseExpense: HandleCloseExpense = () => {
     setIsOpenDialog(false)
   }
 
+  const handleSaveExpense: HandleSaveExpense = () => {
+
+    const updatedBudgets = addedBudgets.map((addedBudget) => {
+      if (addedBudget.name === selectedBudget) {
+
+        const updatedAmount = Number(addedBudget.budget) - Number(expenseAmount);
+
+        return {
+          ...addedBudget,
+          budget: updatedAmount.toString(),
+          expenses: [
+            ...addedBudget.expenses,
+            { description: expenseDescription,
+              amount: expenseAmount}
+          ]
+        };
+      } else {
+        return addedBudget;
+      }
+    });
+
+    console.log("added expenses:", expenses)
+
+    setAddedBudgets(updatedBudgets)
+
+    setExpenseDescription("")
+    setExpenseAmount("")
+    setIsOpenDialog(false)
+  }
+
   return (
     <Container sx={{mt: 5}}>
+
       <CreateBudget
-        setName={setName}
-        setAmount={setAmount}
-        setTotal={setTotal}
         inputAmount={inputAmount}
         inputName={inputName}
         setInputAmount={setInputAmount}
@@ -71,16 +106,19 @@ function App() {
         handleSubmitBudget={handleSubmitBudget}
       />
 
-      {name && amount && (
-        <BudgetCard
-          name={name}
-          amount={amount}
-          total={total}
-          onAddExpense={handleAddExpense}
-          expenses={expenses}
-
+      {addedBudgets.length > 0 && (
+        addedBudgets.map((addedBudget, index) => (
+          <BudgetCard
+            key={index}
+            name={addedBudget.name}
+            amount={addedBudget.budget}
+            total={total}
+            onAddExpense={() => handleAddExpense(addedBudget.name)}
+            expenses={addedBudget.expenses}
         />
+        ))
       )}
+
       <AddExpenseForm
         open={isOpenDialog}
         onCloseExpense={handleCloseExpense}
