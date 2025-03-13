9bb3a378d52f355cf79545058c285f52a4cfcd63
vknanavati <vimin.nanavati@gmail.com>
2025-03-13 14:00:09 -0400

    added code for handleEditExpense, modified handleSaveExpense, handleCloseExpense.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from '@mui/material';
 import { BudgetCard } from './components/BudgetCard';
 import { AddExpenseForm } from './components/AddExpenseForm';
-import { HandleEditExpense, HandleDeleteExpense, HandleAddExpense, HandleCloseExpense, HandleSaveExpense, Expense } from './types';
+import { IsEdit, HandleEditExpense, HandleDeleteExpense, HandleAddExpense, HandleCloseExpense, HandleSaveExpense, Expense } from './types';
 
 function App() {
   //expenseDescription = item name entered by user
@@ -14,41 +14,75 @@ function App() {
   //key are 'description' and 'amount'
   const [expenses, setExpenses] = useState<Expense[]>([])
 
+  const [isEdit, setIsEdit] = useState<IsEdit>(null)
+
   const [isOpenDialog, setIsOpenDialog] = useState(false)
+
+  useEffect(()=>{
+    console.log("updated expenseDescription: ", expenseDescription);
+    console.log("updated expenseAmount: ", expenseAmount)
+    console.log("updated expenses: ", expenses)
+  }, [expenseDescription, expenseAmount, expenses])
 
   const handleAddExpense: HandleAddExpense = () => {
     setIsOpenDialog(true);
   }
 
   const handleCloseExpense: HandleCloseExpense = () => {
-    setIsOpenDialog(false)
+    setIsEdit(null);
+    setIsOpenDialog(false);
   }
 
   const handleSaveExpense: HandleSaveExpense = () => {
+    //edit expense if isEdit not null
+    if (isEdit !== null) {
+      //if index equals isEdit then update that object
+      setExpenses(expenses.map((expense, index) =>
+        //condition is index must equal isEdit. This is how only the selected entry will be updated
+        index === isEdit
+          ? { description: expenseDescription, amount: expenseAmount }
+          : expense
+      ))
+      // setExpenses(updatedExpenses);
+      setIsEdit(null);
 
-    setExpenses((prevExpenses)=> [
-      ...prevExpenses,
-      { description: expenseDescription,
-         amount: expenseAmount
-      }])
+      // console.log("updatedExpenses: ", updatedExpenses);
 
-    console.log("expenses:", expenses)
+    } else {
+      //else add new expense
+      setExpenses((prevExpenses) => [
+        ...prevExpenses,
+        { description: expenseDescription, amount: expenseAmount }
+      ])
+    }
 
-    setExpenseAmount("")
-    setExpenseDescription("")
-    setIsOpenDialog(false)
+    setExpenseAmount("");
+    setExpenseDescription("");
+    setIsOpenDialog(false);
   }
 
-  const handleEditExpense: HandleEditExpense = (description) => {
-    setIsOpenDialog(true)
+  const handleEditExpense: HandleEditExpense = (index) => {
+    //need the index to select the expense to edit
+    //expense is located in an object in an array
+    //selectedEdit is set to object the user wants to edit
+    //selectedEdit variable needed to access description key and amount key in array of expenses object
+    const selectedEdit = expenses[index];
 
+    setExpenseDescription(selectedEdit.description);
+    setExpenseAmount(selectedEdit.amount);
+
+    setIsEdit(index);
+    setIsOpenDialog(true);
+
+    console.log("selected entry to edit and index: ", selectedEdit, index);
   }
 
   const handleDeleteExpense: HandleDeleteExpense = (description) => {
-    setExpenses((prevExpenses) => prevExpenses.filter((expense)=> expense.description !== description))
+    setExpenses((prevExpenses) =>
+      prevExpenses.filter((expense)=>
+        expense.description !== description))
 
-    console.log(`Deleting an expense entry: description sent to function- ${description}`)
-
+    console.log(`Deleting an expense entry: description sent to function- ${description}`);
   }
 
   return (
@@ -67,6 +101,7 @@ function App() {
         setExpenseDescription={setExpenseDescription}
         setExpenseAmount={setExpenseAmount}
         onSaveExpense={handleSaveExpense}
+        isEdit={isEdit}
       />
    </Container>
   );
