d500fd8501938a26a280daf1baec53baf99c6c92
vknanavati <vimin.nanavati@gmail.com>
2025-03-14 07:56:42 -0400

    displayed passed props.

--- src/components/AnnualCard.tsx
+++ src/components/AnnualCard.tsx
@@ -1,6 +1,13 @@
 import { Box, Typography } from '@mui/material';
+import { ExpenseDescription, ExpenseAmount, Expenses  } from '../types';
 
-export function AnnualCard() {
+interface AnnualCardProps {
+  expenseDescription: ExpenseDescription
+  expenseAmount: ExpenseAmount
+  expenses: Expenses
+}
+
+export function AnnualCard({expenses, expenseDescription, expenseAmount}: AnnualCardProps) {
   return (
     <Box
       sx={{
@@ -12,8 +19,14 @@ export function AnnualCard() {
        <Box display={"flex"} justifyContent={"space-around"} gap={5}>
             <Typography variant="h5">Annual Costs </Typography>
         </Box>
-
-
+        <Box>
+          {expenses.map((expense,index)=> (
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <Typography>{expense.description}</Typography>
+              <Typography>${(Number(expense.amount)*12).toFixed(2)}</Typography>
+            </Box>
+          ))}
+        </Box>
     </Box>
   )
 }
