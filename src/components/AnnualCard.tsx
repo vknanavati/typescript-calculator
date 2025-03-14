81d43705acf1e9f443639c03f38ebf970c9dbbb2
vknanavati <vimin.nanavati@gmail.com>
2025-03-14 14:46:19 -0400

    adjusted spacing.

--- src/components/AnnualCard.tsx
+++ src/components/AnnualCard.tsx
@@ -5,8 +5,10 @@ interface AnnualCardProps {
   expenses: Expenses
 }
 
-export function AnnualCard({expenses}: AnnualCardProps) {
+export function AnnualCard({ expenses }: AnnualCardProps) {
+
   return (
+
     <Box
       sx={{
         border: '1px solid ',
@@ -18,10 +20,10 @@ export function AnnualCard({expenses}: AnnualCardProps) {
             <Typography variant="h5">Annual Costs </Typography>
         </Box>
         <Box>
-          {expenses.map((expense,index)=> (
+          {expenses.map((expense, index)=> (
             <Box key={index} display={"flex"} justifyContent={"space-between"}>
               <Typography>{expense.description}</Typography>
-              <Typography>${(Number(expense.amount)*12).toFixed(2)}</Typography>
+              <Typography>${(Number(expense.amount) * 12).toFixed(2)}</Typography>
             </Box>
           ))}
         </Box>
