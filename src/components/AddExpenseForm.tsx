e46c71ca083545a7361f55f3e02168b74e02bdb1
vknanavati <vimin.nanavati@gmail.com>
2025-03-16 12:29:03 -0400

    changed onSaveExpense function setting it to onSubmit instead of onClick.

--- src/components/AddExpenseForm.tsx
+++ src/components/AddExpenseForm.tsx
@@ -19,42 +19,42 @@ interface AddExpenseFormProps {
 
 export function AddExpenseForm({ formSubmitted, expenseAmountError, expenseDescriptionError, handleExpenseDescriptionChange, handleExpenseAmountChange, isEdit, open, onCloseExpense, expenseDescription, expenseAmount, setExpenseDescription, setExpenseAmount, onSaveExpense }: AddExpenseFormProps) {
   return (
-    <Dialog open={open}>
-        <DialogTitle>
-            {isEdit !== null ? "Edit Expense" : "Add Expense"}
-        </DialogTitle>
-        <DialogContent>
-            <Box display={"flex"} gap={6}>
-                <TextField
-                    label={isEdit !==null ? expenseDescription : "Item"}
-                    variant="standard"
-                    value={expenseDescription}
-                    error={!!expenseDescriptionError && formSubmitted}
-                    helperText={formSubmitted && expenseDescriptionError ? expenseDescriptionError: ""}
-                    onChange={handleExpenseDescriptionChange}
-                    // onChange={(e) => setExpenseDescription(e.target.value)}
-                />
-                <TextField
-                    label={isEdit !==null ? expenseAmount : "Amount"}
-                    variant="standard"
-                    value={expenseAmount}
-                    // !! converts expenseAmountError into a boolean
-                    // error prop expects a boolean value
-                    // if expenseAmountError contains error message it is true
-                    // error = {true} only when error message exists
-                    // && formSubmitted so error only shows after form is submitted
-                    error={!!expenseAmountError && formSubmitted}
-                    // error message only appears if formSubmitted true and expenseAmountError exists
-                    // ? expenseAmountError : "" - helper text empty string if expenseAmountError doesn't exist
-                    helperText={formSubmitted && expenseAmountError ? expenseAmountError : ""}
-                    onChange={handleExpenseAmountChange}
-                />
-            </Box>
-        </DialogContent>
-        <DialogActions>
-                <Button onClick={onSaveExpense}>Add</Button>
-                <Button onClick={onCloseExpense}>Cancel</Button>
-        </DialogActions>
+      <Dialog open={open}>
+        <DialogTitle>{isEdit !== null ? "Edit Expense" : "Add Expense"}</DialogTitle>
+        <form onSubmit={onSaveExpense}>
+            <DialogContent>
+                <Box display={"flex"} gap={6}>
+                    <TextField
+                        label={isEdit !==null ? expenseDescription : "Item"}
+                        variant="standard"
+                        value={expenseDescription}
+                        error={!!expenseDescriptionError && formSubmitted}
+                        helperText={formSubmitted && expenseDescriptionError ? expenseDescriptionError: ""}
+                        onChange={handleExpenseDescriptionChange}
+                        // onChange={(e) => setExpenseDescription(e.target.value)}
+                    />
+                    <TextField
+                        label={isEdit !==null ? expenseAmount : "Amount"}
+                        variant="standard"
+                        value={expenseAmount}
+                        // !! converts expenseAmountError into a boolean
+                        // error prop expects a boolean value
+                        // if expenseAmountError contains error message it is true
+                        // error = {true} only when error message exists
+                        // && formSubmitted so error only shows after form is submitted
+                        error={!!expenseAmountError && formSubmitted}
+                        // error message only appears if formSubmitted true and expenseAmountError exists
+                        // ? expenseAmountError : "" - helper text empty string if expenseAmountError doesn't exist
+                        helperText={formSubmitted && expenseAmountError ? expenseAmountError : ""}
+                        onChange={handleExpenseAmountChange}
+                    />
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                    <Button type="submit">Add</Button>
+                    <Button onClick={onCloseExpense}>Cancel</Button>
+            </DialogActions>
+        </form>
     </Dialog>
   )
 }
